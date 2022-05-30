f32899ca9ebc13be7db6a26a780d9fea2e79b940
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-05-30 12:05:35 +0300

    nunjucks configured

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import nunjucks from 'nunjucks'
+const app = express()
+app.set("view engine", "njk")
+
+const env = nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true
+})
+
+env.addGlobal("baseUrl", "https://api.themoviedb.org/3")
+env.addGlobal("apiKey", "4e4e80e882e727e79125d96a8b649773")
+
+app.get('/', function (req, res) {
+    res.render('index.njk');
+})
+
+app.listen(3000)
+console.log('Up on 3000')
